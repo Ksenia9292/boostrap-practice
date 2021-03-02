c24ac3fc2a47259513684f07ca8c2da7ce128aca
Ksenia <ksuna2105@gmail.com>
2021-03-02 19:06:24 +0100

    message

--- script.js
+++ script.js
@@ -61,7 +61,9 @@ $(document).ready(function () {
             }
         });
     }
-
+  $(".navbar_brand").click(function(){
+    window.location.href ="http://127.0.0.1:5500/index.html"
+  });
 
 });
 
