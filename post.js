d9f074b7f6d82530b0b66f4affdb7b85e763b96c
Ksenia <ksuna2105@gmail.com>
2021-03-20 10:51:59 +0100

    new

--- post.js
+++ post.js
@@ -1,59 +1,64 @@
-
+function saveEmail() {
+    var email = $("#email").val()
+    localStorage.setItem("email", email)
+   
+}
 function getPostById(postID) {
     $.ajax({
         url: 'https://jsonplaceholder.typicode.com/posts/' + postID,
         type: "GET",
         success: function (data) {
 
+
             var template = $(`<div class="container">
-             <div class="text-center>
-                        <h1>${data.title}</h1>
-                    </div>
-                    <p>${data.body}</p>
-                </div>
-            </div>`)
+            <div class="name text-center mb-3">
+                <h1 class="heading">${data.title}</h1>
+            </div>
+            <p class="text-justify lead m-3">${data.body}</p>
+        </div>`)
             template.appendTo("#newArticle");
         }
     })
 
 
+
+
+    $(document).ready(function () {
+        var url = new URL(window.location.href)
+        var postID = url.searchParams.get('id')
+        getPostById(postID)
+
+
+        $(".submit").click(saveEmail()) 
+      
+
+
+        // $(function() {
+        //     $("#submit").click(function(e) {
+        //         e.preventDefault()
+        //     var email = $("#email").val()
+        //     window.localStorage.setItem("email", email)
+        //     })
+        // })
+
+
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
 
-
-
-// var template = $(`<article class="blog-post mb-3 shadow">
-//     <div class="row">
-//         <div class="col-sm-4">
-//             <a href="#"><img class="img-fluid" src="img/image4.jpg" alt=""></a>
-//         </div>
-//         <div class="col-sm-8">
-//             <a href="#">
-//                 <h3 class="m-0 mt-2">${data.title}</h3>
-//             </a>
-//             <p class="m-0 nt-2">${data.body}
-//             </p>
-
-//         </div>
-//     </div>
-// </article>`
-
-
-
-
-$(document).ready(function () {
-    var url = new URL(window.location.href)
-    var postID = url.searchParams.get('id')
-    getPostById(postID)
-
-
-    function saveEmail() {
-        var inputEmail = $(".email").val()
-        localStorage.setItem("email", inputEmail)
-       }
-     $(".btn-success").click(saveEmail())
-    
-  
-
-
-})
 
